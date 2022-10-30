167b1174147a5c15651076aedd2f733dd8964964
KTo <kto@lovetou.ru>
2022-10-30 11:49:06 +0700

    Добавил тест

--- lesson-04/unit_tests/test_server.py
+++ lesson-04/unit_tests/test_server.py
@@ -23,34 +23,38 @@ class TestServer(unittest.TestCase):
     ok_dict = {RESPONSE: 200}
 
     def test_ok_check(self):
-        """Корректный запрос"""
+        """ Корректный запрос """
         self.assertEqual(process_client_message(
             {ACTION: PRESENCE, TIME: 1.1, USER: {ACCOUNT_NAME: 'Guest'}}), self.ok_dict)
 
     def test_no_action(self):
-        """Ошибка если нет действия"""
+        """ Ошибка если нет действия """
         self.assertEqual(process_client_message(
             {TIME: '1.1', USER: {ACCOUNT_NAME: 'Guest'}}), self.err_dict)
 
     def test_wrong_action(self):
-        """Ошибка если неизвестное действие"""
+        """ Ошибка если неизвестное действие """
         self.assertEqual(process_client_message(
             {ACTION: 'Wrong', TIME: '1.1', USER: {ACCOUNT_NAME: 'Guest'}}), self.err_dict)
 
     def test_no_time(self):
-        """Ошибка, если запрос не содержит штампа времени"""
+        """ Ошибка, если запрос не содержит штампа времени """
         self.assertEqual(process_client_message(
             {ACTION: PRESENCE, USER: {ACCOUNT_NAME: 'Guest'}}), self.err_dict)
 
     def test_no_user(self):
-        """Ошибка - нет пользователя"""
+        """ Ошибка - нет пользователя """
         self.assertEqual(process_client_message(
             {ACTION: PRESENCE, TIME: '1.1'}), self.err_dict)
 
     def test_unknown_user(self):
-        """Ошибка - не Guest"""
+        """ Ошибка - не Guest """
         self.assertEqual(process_client_message(
             {ACTION: PRESENCE, TIME: 1.1, USER: {ACCOUNT_NAME: 'Guest1'}}), self.err_dict)
+
+    def test_empty_request(self):
+        """ Ошибка - пустой запрос """
+        self.assertEqual(process_client_message(''), self.err_dict)
 
     def tearDown(self) -> None:
         pass
